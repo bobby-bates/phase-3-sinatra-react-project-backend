e77d386a4ceee26ff20fc5bbbe2b912b62e2a066
Bobby Bates <robertfbatesjr@gmail.com>
2022-08-10 18:05:14 -0400

    refactor seed data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get '/' do
-    { message: "Good luck with your project!" }.to_json
+
+  get '/todo' do
+    Todo.all.to_json
   end
 
+  # Make new todo from params
+  post '/todo' do
+    Todo.all.to_json
+  end
+  # Find todo with params
+  patch '/todo/:id' do
+    Todo.all.to_json
+  end
+  
+  # Find todo with params
+  delete '/todo/:id' do
+    Todo.all.to_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-puts "🌱 Seeding spices..."
+puts '🌱 Seeding spices...'
 
 categories = %w[Groceries Research Work Family]
+todos = ['Buy bananas', 'Find the next hot JS framework', 'Make lunch', 'Call Dad']
 
 categories.each { |cat| Category.create({ name: cat }) }
+todos.each_with_index { |todo, i| Todo.create({ body: todo, category: Category.all[i] }) }
 
-Todo.create(body: 'Buy bananas')
-
-puts "✅ Done seeding!"
+puts '✅ Done seeding!'
